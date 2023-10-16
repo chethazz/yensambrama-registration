2782ace3ebf3497b2417e77ede0adfa550e4dc03
MunavvarSinan <munavvarsinan8@gmail.com>
2023-10-17 01:53:23 +0530

    minor changes

--- app/api/test/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
-
-export async function POST(request: NextApiRequest) {
-    return NextResponse.json({ message: 'Hello from the API' })
-}
