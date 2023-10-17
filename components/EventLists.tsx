0a98a9141ef8833148980cb43c0e7fb5a2a949c2
MunavvarSinan <munavvarsinan8@gmail.com>
2023-10-17 10:11:20 +0530

    minor cahnges

--- components/EventLists.tsx
+++ components/EventLists.tsx
@@ -30,7 +30,7 @@ const EventLists = () => {
     const getData = async () => {
         const res = await fetch('https://yensambrama.onrender.com/api/event', {
             cache: 'no-cache',
-            mode: 'cors'
+            mode: 'no-cors',
         })
         console.log(res)
         const data = await res.json()
