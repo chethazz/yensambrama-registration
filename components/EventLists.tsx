61386cd2e72183dd22ce4264d86f450bb21e6c08
MunavvarSinan <munavvarsinan8@gmail.com>
2023-10-18 13:37:17 +0530

    updated

--- components/EventLists.tsx
+++ components/EventLists.tsx
@@ -17,7 +17,6 @@ const EventLists = () => {
     const getData = async () => {
         try {
             setLoading(true)
-            // const res = await fetch('http://localhost:8000/api/event', {
             const res = await fetch('https://yensambrama.onrender.com/api/event', {
                 cache: 'no-cache',
             })
